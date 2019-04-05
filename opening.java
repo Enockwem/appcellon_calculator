4af5c00a69b492968971c8d44a47efdebe9d810e
Enock <enockmale97@gmail.com>
2019-04-05 11:59:22 +0300

    Assignment

---
+++ opening.java
@@ -0,0 +1,9 @@
+public class Testing{
+	public static void main(String[] argv){
+		System.out.println("Step A :Select operation.");
+		System.out.println("%6s","1.Add");
+		System.out.println("%6s","2.Subtract");
+		System.out.println("%6s","3.Multiply");
+		System.out.println("%6s","4.Divide");
+	}
+  }
