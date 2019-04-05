ac4fef3bef8f7b8380bb74a22081f4481488dfdc
Enock <enockmale97@gmail.com>
2019-04-05 15:13:24 +0300

    More correction

--- opening.java
+++ opening.java
@@ -1,16 +1,11 @@
 
 public class Testing{
 	public static void main(String[] argv){
-		System.out.printf("%s %3s\n",Step A",":Select operation.");
-		System.out.printf("%10s\n","1.Add");
-		System.out.printf("%15s\n","2.Subtract");
-		System.out.printf("%15s\n","3.Multiply");
-		System.out.printf("%13s","4.Divide");
-		// Scanner that accepts user input
-		java.util.Scanner input = java.util.Scanner(System.in);
-		System.out.println("Choose an option(1,2,3,4)");
-		int choose = input.nextInt(); 
-
+		System.out.printf("%s %3s\n","Step A",":Select operation.");
+		System.out.printf("%10s\n", "1.Add");
+		System.out.printf("%15s\n", "2.Subtract");
+		System.out.printf("%15s\n", "3.Miltiply");
+		System.out.printf("%13s", "4.Divide");
 		
 		
 	}
