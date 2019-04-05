960d39e936a4136fe6475ddffad8ee04dc4652c2
Enock <enockmale97@gmail.com>
2019-04-05 14:39:34 +0300

    introduced the scanner

--- opening.java
+++ opening.java
@@ -1,3 +1,4 @@
+
 public class Testing{
 	public static void main(String[] argv){
 		System.out.println("Step A :Select operation.");
@@ -5,5 +6,12 @@ public class Testing{
 		System.out.println("%6s","2.Subtract");
 		System.out.println("%6s","3.Multiply");
 		System.out.println("%6s","4.Divide");
+		// Scanner that accepts user input
+		java.util.Scanner input = java.util.Scanner(System.in);
+		System.out.println("Choose an option(1,2,3,4)");
+		int choose = input.nextInt(); 
+
+		
+		
 	}
-  }
+}
