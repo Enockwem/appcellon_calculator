3f05ee50f50d1a5e0307401cb88cf76bc0a5dd47
Enock <enockmale97@gmail.com>
2019-04-05 15:22:36 +0300

    Trying

--- opening.java
+++ opening.java
@@ -1,6 +1,6 @@
 
 public class Testing{
-	public static void main(String[] argv){
+	public static void main(String[] args){
 		System.out.printf("%s %3s\n","Step A",":Select operation.");
 		System.out.printf("%10s\n", "1.Add");
 		System.out.printf("%15s\n", "2.Subtract");
