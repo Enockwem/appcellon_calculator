30b7adc2fc625b58aaf765033683456bfce3cdb0
Enock <enockmale97@gmail.com>
2019-04-08 16:04:06 +0300

    Class

---
+++ src/appcellon_calculator/Testing.java
@@ -0,0 +1,17 @@
+package appcellon_calculator;
+
+public class Testing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.printf("%s %3s\n","Step A",":Select operation.");
+		System.out.printf("%10s\n", "1.Add");
+		System.out.printf("%15s\n", "2.Subtract");
+		System.out.printf("%15s\n", "3.Miltiply");
+		System.out.printf("%13s", "4.Divide");
+		//Scanner object
+		java.util.Scanner scan = new java.util.Scanner(System.in);
+		System.out.println("Enter your Choice here");
+	}
+
+}
